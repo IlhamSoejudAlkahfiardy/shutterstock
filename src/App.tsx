bd1845986c2ff8f139eb04a9862c61eb718effd5
IlhamSoejudAlkahfiardy <ilhamyusuf386@gmail.com>
2026-07-14 10:12:26 +0700

    feat: integrate Vercel analytics and speed insights
    Add @vercel/analytics and @vercel/speed-insights packages to enhance application performance monitoring and analytics. Update package.json and package-lock.json to include new dependencies, and modify App component to incorporate Analytics and SpeedInsights components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { AppLayout } from "@/layouts/AppLayout";
 import { HomePage } from "@/pages/HomePage";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
-    <AppLayout>
-      <HomePage />
-    </AppLayout>
+    <>
+      <AppLayout>
+        <HomePage />
+      </AppLayout>
+      <Analytics />
+      <SpeedInsights />
+    </>
   );
 }
 
